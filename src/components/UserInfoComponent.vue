c70bb7ab582bbe87f38bbf6365896ddb1949b001
Nico Piovano <npiovano@sugit.com.ar>
2024-06-27 15:42:39 -0300

    User info

--- src/components/UserInfoComponent.vue
+++ src/components/UserInfoComponent.vue
@@ -6,38 +6,8 @@
     <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
       <q-list padding>
         <q-item clickable v-ripple>
-          <q-item-section avatar>
-            <q-icon name="inbox" />
-          </q-item-section>
-
-          <q-item-section>
-            Inbox
-          </q-item-section>
-        </q-item>
-
-        <q-item active clickable v-ripple>
-          <q-item-section avatar>
-            <q-icon name="star" />
-          </q-item-section>
-
-          <q-item-section>
-            Star
-          </q-item-section>
-        </q-item>
-
-        <q-item clickable v-ripple>
-          <q-item-section avatar>
-            <q-icon name="send" />
-          </q-item-section>
-
-          <q-item-section>
-            Send
-          </q-item-section>
-        </q-item>
-
-        <q-item clickable v-ripple>
           <q-btn label="Salir"
-                 v-if="userGoogle"
+                 v-if="auth"
                  color="negative"
                  @click="logoutGoogle" />
         </q-item>
@@ -47,11 +17,10 @@
     <q-img class="absolute-top" src="https://cdn.quasar.dev/img/material.png" style="height: 150px">
       <div class="absolute-bottom bg-transparent">
         <q-avatar size="56px" class="q-mb-sm">
-          <img alt="" src="https://cdn.quasar.dev/img/boy-avatar.png">
+          <img alt="" :src="auth.currentUser.providerData[0].photoURL">
         </q-avatar>
-        <div class="text-weight-bold">Razvan Stoenescu</div>
-        <div>@rstoenescu</div>
-
+        <div class="text-weight-bold">{{ auth.currentUser.displayName }}</div>
+        <div>{{ auth.currentUser.email }}</div>
       </div>
     </q-img>
 
@@ -60,11 +29,10 @@
 </template>
 
 <script setup>
-import { inject } from "vue";
 import { signOut } from "firebase/auth";
 import { auth } from "src/firebase";
 
-const userGoogle = inject("userGoogle");
+console.log();
 const logoutGoogle = () => {
   signOut(auth)
     .catch(err => console.log(err));
